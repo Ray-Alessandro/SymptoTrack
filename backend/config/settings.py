eeefe1e02e139e8a02790a8eecf13b5c20ea5298
Ray <ray_punkeki@hotmail.com>
2024-06-07 21:14:38 -0500

    feat: backend's structure configured

---
+++ backend/config/settings.py
@@ -0,0 +1,6 @@
+class Settings:
+    app_name = "SymptoTrackApi"
+    app_version = "1.0"
+    app_description = "This is the API for the SymptoTrack project"
+    
+settings = Settings()
---
+++ backend/models/response/DiseaseResponse.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from typing import List
+
+from SymptomResponse import SymptomResponse
+
+
+class DiseaseResponse(BaseModel):
+    name: str
+    description: str
+    image: str
+    percentage: float
+    symptoms: List[SymptomResponse]
